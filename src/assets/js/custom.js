136f9b92277b05827238153eed0dc326b8d1ff0a
mlbcreative <charles@mlbcreative.com>
2017-10-26 10:18:34 -0400

    updated custom.js with total donation amount logic

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -23,12 +23,20 @@ $('.cci-card').on('mouseenter', function(){
 
 //add comma to amounts > 1000
 
-var total = parseInt($('#campaign-donations').text());
+var total = $('#campaign-donations').text();
 
-$('#total-funds-amt-header').text(total.toLocaleString());
-$('#total-funds-amt').text("$" + total.toLocaleString());
+//var total = $('#total-funds-total').text();
+if(total != "0") {
+	total = total.slice(0,-2);
+	total = parseInt(total);
 
-$('.story a').on('click',function(e){
-	e.preventDefault();
-	//
-})
+	$('#total-funds-amt').empty().text(total.toLocaleString());
+	$('#total-funds-amt-header').text("$" + total.toLocaleString());
+	
+} else if(total == "0") {
+	$('#total-funds-amt').empty().text(total);
+	$('#total-funds-amt-header').empty().text(total);
+} else {
+	$('#total-funds-amt').empty().text("0");
+	$('#total-funds-amt-header').empty().text("0");
+}
